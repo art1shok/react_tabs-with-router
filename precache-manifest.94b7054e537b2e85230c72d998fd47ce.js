75a09be0c2381df540d70f2a2d96a0cfd968f4d4
art1shok <kvitkaart554@gmail.com>
2020-01-02 09:55:24 +0200

    Updates

---
+++ precache-manifest.94b7054e537b2e85230c72d998fd47ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e48c3a148cf4bdac9b4d6b6f9d1335",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "ea72de8beaae6f003b8f",
+    "url": "/react_tabs-with-router/static/css/main.d4e556e7.chunk.css"
+  },
+  {
+    "revision": "a6ed9298720ddeba0193",
+    "url": "/react_tabs-with-router/static/js/2.605c7544.chunk.js"
+  },
+  {
+    "revision": "ea72de8beaae6f003b8f",
+    "url": "/react_tabs-with-router/static/js/main.23cd90a1.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
